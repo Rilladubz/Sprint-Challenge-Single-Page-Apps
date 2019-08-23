b05bc4493de052b190956a5672cae083171a3eda
Samuel Torres <rilla@Samuels-iMac.local>
2019-08-23 13:41:56 -0400

    MVP

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 
+function handleChange (e){
+
+}
+
 const panes = [
-    { menuItem: 'Home Page', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-    { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    { menuItem: 'Home Page', render: () => <NavLink to='/'><Tab.Pane>CLICK HERE FOR HOME PAGE</Tab.Pane></NavLink>},
+    { menuItem: 'Characters', render: () => <NavLink to='/characters'><Tab.Pane>CLICK HERE FOR CHARACTERS PAGE</Tab.Pane></NavLink> },
+    { menuItem: 'Locations', render: () => <NavLink to='/locations'> <Tab.Pane>CLICK HERE FOR LOCATIONS PAGE</Tab.Pane></NavLink>},
+    { menuItem: 'Episodes', render: () => <NavLink to='/episodes'><Tab.Pane>CLICK HERE FOR EPISODES PAGE</Tab.Pane></NavLink> },
   ]
   
   const TabExampleBasic = () => <Tab panes={panes} />
